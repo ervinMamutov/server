dc072ef22fc5c46157be35f15debf7e4031c00a0
ervinMamutov <ervin.news@gmail.com>
2023-10-26 09:29:02 +0200

    added email validation

--- utils/validateEmail.js
+++ utils/validateEmail.js
@@ -1,3 +1,6 @@
 const validateEmail = (email) => {
-  const pattern = 
-}
+  const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return pattern.test(email) ? true : false;
+};
+
+export default validateEmail;
